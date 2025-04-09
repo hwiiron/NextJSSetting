bb5c62fcde3c82a348966c950f2668ddc4feb41f
HwichulShin <shinhwiiron@gmail.com>
2025-04-09 11:11:39 +0900

    Apply Pretend Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        {/* Pretendard Font CDN */}
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
+          crossOrigin="anonymous"
+        />
+      </head>
+
       <body>{children}</body>
     </html>
   );
