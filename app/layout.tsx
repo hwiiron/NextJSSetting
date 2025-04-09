d303ea1bbd2c48f313f956406534fcf6af306542
HwichulShin <shinhwiiron@gmail.com>
2025-04-09 11:38:21 +0900

    Description Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Next.js Project Setting",
   description:
-    "Next.js(React 18 Version), TypeScript, Tailwind(3 Version), Prettier, ESLint Install & Setting",
+    "Next.js(React 18 Version, App Router), TypeScript, Tailwind(3 Version), Prettier, ESLint Install & Setting",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ function Home() {
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
       <h1 className="mb-4 text-2xl">Project Setting</h1>
       <p>
-        Next.js(React 18 Version), TypeScript, Tailwind(3 Version),
+        Next.js(React 18 Version, App Router), TypeScript, Tailwind(3 Version),
         <br />
         Prettier, ESLint Install & Setting
       </p>
